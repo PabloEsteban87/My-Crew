abc645314af095f20a88352a27aa5b7c6858b86f
PabloEsteban87 <pablosebas87@gmail.com>
2023-05-21 16:47:46 +0200

    proyecto_acabado

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function emoji_triste() {
+let feliz_1 = document.getElementById("feliz");
+let triste_1 = document.getElementById("triste");
+feliz_1.style.display = "none";
+triste_1.style.display = "block";
+}
